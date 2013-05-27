7d74c687d8806604f4d9af70e73b611f4f367fe6
Joscha Krutzki <joka@jokasis.de>
2013-05-27 15:39:50 +0200

    updated reset database script

--- scripts/reset_database_with_testdata.sh
+++ scripts/reset_database_with_testdata.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+#bin/supervisorctl stop webshop_api
+#cp etc/webshop_database_with_testdata.fs var/webshopapi_database/Data.fs 
+#bin/supervisorctl start webshop_api
+
 bin/mysql -D magento < etc/magento_database_basic_views_WITH_TESTDATA_16_05_13.sql
